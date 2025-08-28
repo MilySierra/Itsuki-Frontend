12a78a71fcba873a9d8e389f13b5feecdc7d8c06
Mily <mily.sierra@udea.edu.co>
2025-08-28 01:53:07 -0500

    Feature: Primer commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Itsuki-Frontend',
+  webDir: 'www'
+};
+
+export default config;
